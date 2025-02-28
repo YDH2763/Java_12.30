e791bdf63c8be3b5f7e3fb344ce412c303d47623
YDH2763 <dbehdgns12345@gmail.com>
2025-02-28 16:46:01 +0900

    40일차 수업(02월 28일)

---
+++ DB/15_쇼핑몰 DML.sql
@@ -0,0 +1,17 @@
+# 회원이 다음의 정보로 회원 가입을 했을 때 필요한 쿼리
+# 아이디 : abc123, 비밀번호 : abc123, 이메일 : abc123@db.com, 전화번호 :111-1234-5678
+
+INSERT INTO MEMBER (ME_ID, ME_PW, ME_EMAIL, ME_NUMBER)
+VALUE("abc123", "abc123", "abc123@db.com", "111-1234-5678");
+ 
+ # 관리자가 다음 정보로 회원 가입을 했을 때 필요한 쿼리
+ # 아이디 : admin123, 비밀번호 : admin123, 이메일 : admin123@db.com, 전화번호 :111-2222-3333
+ 
+INSERT INTO MEMBER (ME_ID, ME_PW, ME_EMAIL, ME_NUMBER, ME_AUTHORITY)
+VALUE("admin123", "admin123", "admin123@db.com", "111-2222-3333", "ADMIN");
+
+# 카테고리를 등록할 때 필요한 쿼리
+# 상의 :TOP, 하의 : PAN, 악세서리 : ACC, 모자 : CAP, 신발 : SHO
+
+INSERT INTO CATEGORY(CA_NAME, CA_CODE) VALUES
+("상의","TOP"),("하의","PAN"),("악세서리","ACC"),("모자","CAP"),("신발","SHO");

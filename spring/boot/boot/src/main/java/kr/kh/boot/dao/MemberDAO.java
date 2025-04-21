3b7023713fc9ffd1fae303d631cd7c61e1f1b16a
YDH2763 <dbehdgns12345@gmail.com>
2025-04-21 16:34:30 +0900

    76일차 오후수업(스프링 부트 기초)(04월 21일)

---
+++ spring/boot/boot/src/main/java/kr/kh/boot/dao/MemberDAO.java
@@ -0,0 +1,7 @@
+package kr.kh.boot.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public interface MemberDAO {
+	
+}
---
+++ spring/boot/boot/src/main/java/kr/kh/boot/service/PostService.java
@@ -0,0 +1,21 @@
+package kr.kh.boot.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.kh.boot.dao.PostDAO;
+import kr.kh.boot.model.vo.PostVO;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	PostDAO postDao;
+
+	public List<PostVO> getPostList(int bo_num) {
+		
+		return postDao.selectPostList(bo_num);
+	}
+}
